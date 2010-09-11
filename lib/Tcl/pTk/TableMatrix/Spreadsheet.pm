73b293b523e44fbd1e95aee9e1b1c5e8127d23ad
John Cerney <j-cerney1@raytheon.com>
2010-09-11 17:30:18 +0000

    Fixed bug where hitting right-arrow when editing a cell would move the insertion cursor the wrong way (left).
    git-svn-id: https://svn.code.sf.net/p/perl-tcl-ptk/code/trunk@172 8ab6fd2f-3ca1-42eb-bd32-3378fe2cd338

--- lib/Tcl/pTk/TableMatrix/Spreadsheet.pm
+++ lib/Tcl/pTk/TableMatrix/Spreadsheet.pm
@@ -251,7 +251,7 @@ sub ClassInit{
                     my $active = eval { $w->index('active'); }; 
                     if( $active  && $active ne '0,0' ){
                             my $posn = $w->icursor;
-                            $w->icursor($posn - 1);
+                            $w->icursor($posn + 1);
                     }
                     else{
                             # Not editing a cell, just move the selected cell around
